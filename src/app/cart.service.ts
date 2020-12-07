24e9d4fb54534f095a11085cd3fe49c0cb1f6ebf
AdiLakshmi-Chowdam <chowdamadilakshmi@gmail.com>
2020-12-07 12:03:55 +0530

    4 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,21 +1,28 @@
-import { Injectable } from "@angular/core";
+import { Injectable, Inject } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { LOCAL_STORAGE, StorageService } from "ngx-webstorage-service";
 
 @Injectable()
 export class CartService {
   items = [];
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(LOCAL_STORAGE) private storageService: StorageService
+  ) {}
 
   addToCart(product) {
     this.items.push(product);
+    this.storageService.set("myCart", this.items);
   }
 
   getItems() {
+    this.storageService.get("myCart");
     return this.items;
   }
 
   clearCart() {
     this.items = [];
+    this.storageService.remove("myCart");
     return this.items;
   }
 
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Inject } from "@angular/core";
 import { CartService } from "../cart.service";
-
+import { Router } from "@angular/router";
+import { LOCAL_STORAGE, StorageService } from "ngx-webstorage-service";
 @Component({
   selector: "app-cart",
   templateUrl: "./cart.component.html",
@@ -8,9 +9,23 @@ import { CartService } from "../cart.service";
 })
 export class CartComponent implements OnInit {
   items = [];
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private router: Router,
+    @Inject(LOCAL_STORAGE) private storageService: StorageService
+  ) {}
 
   ngOnInit() {
-    this.items = this.cartService.getItems();
+    this.items = this.storageService.get("myCart");
+    //this.items = this.cartService.getItems();
+  }
+  routeTo500() {
+    this.router.navigateByUrl("internal-server-error");
+  }
+
+  removeItem(productNumber) {
+    this.items.splice(productNumber, 1);
+    this.storageService.set("myCart", this.items);
+    this.items = this.storageService.get("myCart");
   }
 }
